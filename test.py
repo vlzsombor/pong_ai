641215e06286c85dc5b9b420dc21f57c3990d040
Zsombor Veres-Lakos <zsombor.veres-lakos@ilogs.care>
2024-12-29 11:55:08 +0100

    implement ai control

---
+++ test.py
@@ -0,0 +1,25 @@
+
+
+
+# def hello(x):
+#     print('hello')
+#     print(x)
+    
+
+
+# def methodRunner(m):
+#     m()
+    
+# methodRunner(lambda: hello("xdxdxd"))
+
+
+# genomes = [(1, "genomeA"), (2, "genomeB"), (3, "genomeC")]
+
+# for i, (genome_id1, genome1) in enumerate(genomes):
+#     print(f"Index: {i}, Genome ID: {genome_id1}, Genome: {genome1}")
+
+
+# print(enumerate(genomes)[0])
+
+b = 1==1 | 2 == 1
+print(b)
--- tutorial.py
+++ tutorial.py
@@ -1,32 +1,118 @@
 import pygame
 from pong import Game
+import os
+import neat
 
 width, height = 600, 400
-
 window = pygame.display.set_mode((width, height))
 
-game = Game(window, width, height)
-run = True
 
-clock = pygame.time.Clock()
+class PongGame:
+    def __init__(self, window, width, height):
+        self.game = Game(window, width, height)
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+        self.ball = self.game.ball
 
-while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    def test_ai(self):
+        game = Game(window, width, height)
+        run = True
+
+        clock = pygame.time.Clock()
+
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_w]:
+                game.move_paddle(left=True, up = True)
+            if keys[pygame.K_s]:
+                game.move_paddle(left=True, up = False)
+
+            game_info = game.loop()
+            print(game_info.left_score, game_info.right_score)
+            game.draw(False, True)
+            pygame.display.update()
+    def train_ai(self, genome1, genome2, config):
+        net1 = neat.nn.FeedForwardNetwork.create(genome1, config)
+        net2 = neat.nn.FeedForwardNetwork.create(genome2, config)
+
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()            
+            game_info = self.game.loop()
+            self.game.draw()
+            pygame.display.update()
+            output1 = net1.activate((self.left_paddle.y, self.ball.y, abs(self.left_paddle.x - self.ball.x)))
+            decision1 = output1.index(max(output1))
+            
+
+            output2 = net2.activate((self.right_paddle.y, self.ball.y, abs(self.right_paddle.x - self.ball.x)))
+            decision2 = output2.index(max(output2))
+
+            if decision1 == 0:
+                pass
+            elif decision1 == 1:
+                self.game.move_paddle(left=True, up=True)
+            else:
+                self.game.move_paddle(left=True, up=False)
+            
+            if decision2 == 0:
+                pass
+            elif decision2 == 1:
+                self.game.move_paddle(left=False, up=True)
+            else:
+                self.game.move_paddle(left=False, up=False)
+
+            print(output1)
+            print(output2)
+            #58:15
+            if game_info.left_score >= 1 or game_info.right_score >= 1:
+                self.calculate_fitness(genome1, genome2, game_info)
+                break
+    def calculate_fitness(self, genome1, genome2, game_info):
+        pass
+
+def eval_genomes(genomes, config):
+    window = pygame.display.set_mode((width, height))
+
+
+    for i, (genome_id1, genome1) in enumerate(genomes):
+        if i == len(genomes) - 1:
             break
+        genome1.fitness = 0
+        for genome_id2, genome2 in genomes[i+1:]:
+            genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
+            game = PongGame(window, width, height)
+            game.train_ai(genome1, genome2, config)    
+
+
+def run_neat(config):
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(1))
+
+    winner = p.run(eval_genomes, 50)
+
+#game = PongGame(window, width, height)
+#game.test_ai()
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir,"config.txt")
     
-    keys = pygame.key.get_pressed()
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+    run_neat(config)
 
-    if keys[pygame.K_w]:
-        game.move_paddle(left=True, up = True)
-    if keys[pygame.K_s]:
-        game.move_paddle(left=True, up = False)
 
-    game_info = game.loop()
-    print(game_info.left_score, game_info.right_score)
-    game.draw(False, True)
-    pygame.display.update()
-
-print('hello')
+    
